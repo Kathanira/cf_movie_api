2863865699c08a938ef1526bd47d79b6b7acb2a3
Kathanira <112494140+Kathanira@users.noreply.github.com>
2022-11-24 12:26:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
   });
 
   // Allow users add to their list of Favorites (create)
-  app.put('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
+  app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
     Users.findOneAndUpdate({ Username: req.params.Username }, {
       $push: { FavoriteMovies: req.params.MovieID }
     },
